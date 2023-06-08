896855915a60c541be76e2f17a44b1cadf39e3aa
Henrik Nordtorp <105918296+hnordto@users.noreply.github.com>
2023-06-08 16:01:14 +0200

    Add figures folder and minor bug fixes

--- dev/discharge_intervals.R
+++ dev/discharge_intervals.R
@@ -8,7 +8,12 @@ discharge.east = filter_region(discharge)
 discharge.east.long = make_rec_discharge(discharge.east, reshape = T)
 
 # A lot of missing data points
-plot_rec_discharge(discharge.east.long)
+p = plot_rec_discharge(discharge.east.long)
+p
+
+ggsave(p,
+       filename = "discharge_rec_east.png",
+       path = "//ad.nr.no/shares/samba_shared/Sommerstudenter/Henrik/figures")
 
 # Subset
 discharge.data = subset_discharge(discharge.east,
@@ -16,7 +21,13 @@ discharge.data = subset_discharge(discharge.east,
                                   day_threshold = 364)
 
 # No missing data points!
-plot_rec_discharge(make_rec_discharge(discharge.data, reshape = T))
+p = plot_rec_discharge(make_rec_discharge(discharge.data, reshape = T))
+p
+
+ggsave(p,
+       filename = "discharge_rec_east_processed.png",
+       path = "//ad.nr.no/shares/samba_shared/Sommerstudenter/Henrik/figures")
+
 
 
 
