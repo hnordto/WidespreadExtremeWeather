b09946d8dc01907b84202491b730c2260e83647f
Henrik Nordtorp <105918296+hnordto@users.noreply.github.com>
2023-06-12 11:55:56 +0200

    Add generalized functions for identifying main events and creating binary event matrices.

--- R/main_functions.R
+++ R/main_functions.R
@@ -46,3 +46,72 @@ extreme_events = function(data,
 time_threshold = function(deltaT) {
   return(deltaT)
 }
+
+main_events = function(extreme.events,
+                       day_threshold = 7) {
+  event.frequencies = extreme.events[,.(Freq = .N), by = date]
+  setorder(event.frequencies, cols = - Freq)
+  
+  uniqueevents = as.character(event.frequencies$date)
+  
+  for (i in 1:length(uniqueevents)) {
+    event = event.frequencies[date == uniqueevents[i]]
+    
+    event.freq = event$Freq
+    
+    event.range.lower = as.Date(event$date) - days(day_threshold)
+    event.range.upper = as.Date(event$date) + days(day_threshold)
+    event.range = seq(event.range.lower, event.range.upper, by = 1)
+    
+    proximity.events = event.frequencies[date %in% event.range]
+    alternative.event = proximity.events[Freq == event.freq]
+    
+    event.to.keep = quantile(alternative.event$date, p = .5, type = 1)
+    event.to.keep = as.Date(event.to.keep)
+    
+    if (i == 1) {
+      main.events = event.to.keep
+    } else {
+      if (!any(abs(difftime(event.to.keep, main.events, units = "days")) < day_threshold)) {
+        main.events = c(main.events, event.to.keep)
+      }
+    }
+  }
+  
+  return(main.events)
+  
+}
+
+event_matrix = function(main.events,
+                        extreme.events,
+                        deltaT = 2) {
+  
+  allstations = unique(extreme.events$stat_id)
+  
+  mat = matrix(data = NA,
+               nrow = length(allstations),
+               ncol = length(main.events))
+  
+  rownames(mat) = allstations
+  colnames(mat) = as.character(main.events)
+  
+  for (i in 1:length(main.events)) {
+    this.event = main.events[i]
+    event.range.lower = as.Date(this.event) - days(deltaT)
+    event.range.upper = as.Date(this.event) + days(deltaT)
+    event.range = seq(event.range.lower, event.range.upper, by = 1)
+    
+    for (j in 1:length(allstations)) {
+      this.station = extreme.events[stat_id == allstations[j]]
+      
+      if (any(this.station$date %in% event.range)) {
+        mat[j, i] = 1
+      } else {
+        mat[j, i] = 0
+      }
+    }
+  }
+  
+  return(mat)
+  
+}
