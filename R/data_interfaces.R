12d68dd34584cc2a8779fc8bc4037a923dae7830
Henrik Nordtorp <105918296+hnordto@users.noreply.github.com>
2023-07-03 09:13:27 +0200

    Update interfaces

--- R/data_interfaces.R
+++ R/data_interfaces.R
@@ -1,7 +1,17 @@
-load_data <- function(type = "discharge") {
+load_data <- function(type = "both") {
   if (type == "discharge") {
     load("data/nvedat.RData")
     l = list(discharge = nvedat)
+    return (l)
+  } else if (type == "precip") {
+    load("data/metdat.RData")
+    l = list(precip = metdat)
+    return (l)
+  } else if (type == "both") {
+    load("data/nvedat.RData")
+    load(data/metdat.RData)
+    l = list(discharge = nvedat,
+             precip = metdat)
     return (l)
   } else {
     return (NULL)
